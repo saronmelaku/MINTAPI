797b924a628fd1b37de7ea405615636f6e5faf97
saronmelaku <saronmelaku@gmail.com>
2020-10-29 20:14:56 +0300

    first commit

--- Model/Course.cs
+++
@@ -1,8 +0,0 @@
-namespace MINTAPI.Model {
- public class Course {
-    
-        public string CourseNo { get; set; }
-        public string courseTitle { get; set; }
-        public int CreiditHour { get; set; }
-    }
-}
--- Model/Department.cs
+++
@@ -1,9 +0,0 @@
-namespace MINTAPI.Model
-{
-    public class Department
-    {
-        public int DepartmentID { get; set; }
-        public string DepartmentName { get; set; }
-        public string Faculty { get; set; }
-    }
-}
